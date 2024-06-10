84792600e6a062a355e27ea7bd38bae42fed5068
Arthur Rodrigues de Lima <126973735+Arthursoueu@users.noreply.github.com>
2024-06-10 09:36:14 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    question: "Você conhece a vitima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Esteve no local da ocorrência?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Mora perto da vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Já trabalhou com a vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você devia para vítima vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você tem algum parentesco com a vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você já teve algum relacionamento com a vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você telefonou para a vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você viu a vitima no dia?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+  {
+    question: "Você teve algum tipo de desentendimento com a vítima?",
+    answers: [
+      { option: "sim", correct: true },
+      { option: "não", correct: false },
+    ],
+  },
+];
